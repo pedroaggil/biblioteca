0d0f0344a577579fe796d3118bdc36dc9b682c75
Pedro Gil <pedroaggil@gmail.com>
2022-11-01 16:28:28 -0300

    Delete update book system

--- script/availability.php
+++ script/availability.php
@@ -6,11 +6,17 @@
                 FROM tb_book
                 WHERE id_book = ". $book;
 
-    if ($src__sql = $mysqli->query($search)) {
+    $sql = $mysqli->query($search);
+
+   if ($src__sql = $sql -> fetch_object()) {
+        $a = $src__sql->st_availability;
+        echo $a;
         if ($src__sql->st_availability == 'Sim') {
             $new__availability = 'Não';
         } elseif ($src__sql->st_availability == 'Não') {
             $new__availability = 'Sim';
+        } else {
+            echo 'me diz o que te ofende amigo PORFAVOR';
         }
     }
 
@@ -18,10 +24,12 @@
                 SET st_availability = '".$new__availability."' 
                 WHERE id_book = ". $book;
 
-    if ($upd__sql = $mysqli->query($update)) {
+    echo $update;
+  
+    /* if ($upd__sql = $mysqli->query($update)) {
         header('Location: ../templates/admin/index.php');
     } else {
         echo $mysqli->error;
     }
-
+*/
 ?>
